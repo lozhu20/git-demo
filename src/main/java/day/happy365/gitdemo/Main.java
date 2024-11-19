f9762ba71756afd94e263cd60ac189baf7d92ebc
lozhu <zhulongkun20@icloud.com>
2024-11-19 21:47:31 +0800

    功能1开发完成

--- src/main/java/day/happy365/gitdemo/Main.java
+++ src/main/java/day/happy365/gitdemo/Main.java
@@ -3,4 +3,8 @@ package day.happy365.gitdemo;
 public class Main {
     public static void main(String[] args) {
     }
+
+    private static void feature1() {
+        System.out.println("feature1");
+    }
 }
